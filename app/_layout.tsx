2e3c242c318c0612445b48caa98c7b6686245cb9
Sadman Yasar Sayem <sadmanyasarsayem@gmail.com>
2023-08-29 09:35:16 +0800

    test husky

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -18,36 +18,43 @@ import { Stack, SplashScreen } from 'expo-router';
 
 import { Suspense } from 'react';
 import { useColorScheme } from 'react-native';
-import { Button, Paragraph, TamaguiProvider, Theme, YStack, Text } from 'tamagui';
+import {
+  Button,
+  Paragraph,
+  TamaguiProvider,
+  Theme,
+  YStack,
+  Text,
+} from 'tamagui';
 
 import { MySafeAreaView } from '../components/MySafeAreaView';
 import config from '../tamagui.config';
 
 export default function StackLayout() {
-    const colorScheme = useColorScheme();
-    const [loaded] = useFonts({
-        Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+  const colorScheme = useColorScheme();
+  const [loaded] = useFonts({
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
 
-        InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
-    });
-    if (!loaded) {
-        return null;
-    }
-    return (
-        <TamaguiProvider config={config}>
-            <Suspense fallback={<Text>Loading...</Text>}>
-                <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
-                    <MySafeAreaView>
-                        <YStack f={1} jc="center" ai="center" backgroundColor="$background">
-                            <Paragraph color="$color" jc="center">
-                                {colorScheme} hello
-                            </Paragraph>
-                            <Button>Button</Button>
-                            {/* <StatusBar style="auto" /> */}
-                        </YStack>
-                    </MySafeAreaView>
-                </Theme>
-            </Suspense>
-        </TamaguiProvider>
-    );
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+  if (!loaded) {
+    return null;
+  }
+  return (
+    <TamaguiProvider config={config}>
+      <Suspense fallback={<Text>Loading...</Text>}>
+        <Theme name={colorScheme === 'dark' ? 'dark' : 'light'}>
+          <MySafeAreaView>
+            <YStack f={1} jc="center" ai="center" backgroundColor="$background">
+              <Paragraph color="$color" jc="center">
+                {colorScheme} hello
+              </Paragraph>
+              <Button>Button</Button>
+              {/* <StatusBar style="auto" /> */}
+            </YStack>
+          </MySafeAreaView>
+        </Theme>
+      </Suspense>
+    </TamaguiProvider>
+  );
 }
